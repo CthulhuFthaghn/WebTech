cee77babaa9994fc1230640b7262b51389d34f74
CthulhuFthaghn <109551124+CthulhuFthaghn@users.noreply.github.com>
2024-04-25 02:40:02 +0200

    Update fruit_script.js
    Trying to solve the problem

--- Guess_The_Fruit/fruit_script.js
+++ Guess_The_Fruit/fruit_script.js
@@ -16,6 +16,7 @@ function CheckTheFruitSubmitted(){
     console.log("in");
     if(AppleShadow.src != "../GuessTheFruitImg/AppleShadow.png")
     {
+        console.log("On a return");
         return; //Make the button spamming impossible as long as a fruit is revealed
     }
     Selection = document.getElementById("FruitSelection").value;
@@ -52,10 +53,11 @@ function CheckTheFruitSubmitted(){
     }
     console.log(GoofyFruit)
 
-    
+    console.log("L'image va être changée");
     var MomentaryPicture = GoofyFruit[getRandomInt(4)];
     AppleShadow.src = "../GuessTheFruitImg/" + MomentaryPicture.toString();
     GoofyFruit = ["AppleKiwi.png", "AppleBanana.png", "AppleOrange.png", "ApplePear.png","Apple.png"]; //We make sure the list is still full
+    console.log("L'image a changée");
     return; 
 }
 
