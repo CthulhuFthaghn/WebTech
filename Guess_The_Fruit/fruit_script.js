2b4948b98744eed8cfb1770816c8840b92c848a6
CthulhuFthaghn <109551124+CthulhuFthaghn@users.noreply.github.com>
2024-04-25 02:12:30 +0200

    Update fruit_script.js

--- Guess_The_Fruit/fruit_script.js
+++ Guess_The_Fruit/fruit_script.js
@@ -13,8 +13,8 @@ function getRandomInt(max) {
 
 function CheckTheFruitSubmitted(){
     AppleShadow = document.getElementById("AppleShadow");
-    alert("We are in the function");
-    if(AppleShadow.src != "\GuessTheFruitImg/AppleShadow.png")
+    console.log("in");
+    if(AppleShadow.src != "GuessTheFruitImg/AppleShadow.png")
     {
         return; //Make the button spamming impossible as long as a fruit is revealed
     }
@@ -54,7 +54,7 @@ function CheckTheFruitSubmitted(){
 
     
     var MomentaryPicture = GoofyFruit[getRandomInt(4)];
-    AppleShadow.src = "\GuessTheFruitImg/" + MomentaryPicture.toString();
+    AppleShadow.src = "GuessTheFruitImg/" + MomentaryPicture.toString();
     GoofyFruit = ["AppleKiwi.png", "AppleBanana.png", "AppleOrange.png", "ApplePear.png","Apple.png"]; //We make sure the list is still full
     return; 
 }
